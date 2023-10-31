8b9626258aca60275601401b86d88b158c9113e0
Adam Cole <adamcole123@hotmail.co.uk>
2023-10-31 23:43:23 +0000

    Added suggested interface

---
+++ CheckoutKata/CheckoutKata.Library/ICheckout.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.Library
+{
+    internal interface ICheckout
+    {
+        void Scan(string item);
+        int GetTotalPrice();
+    }
+}
